5bb5ace834bca2d0756b2d0ae5ac0c3f7683b090
gabrielhribeiro <gabrieldavid@outlook.com>
2014-11-01 15:06:18 -0200

    Update local server

---
+++ README.md
@@ -0,0 +1,14 @@
+Steganocrypto
+==============
+Steganography( LSB IMAGES ) using AES Cryptography together
+
+Python program to steganography files into images using the Least Significant Bit technics and Advanced Encryption Standard(AES).
+
+Steganocrypto use a graphical interface to improve the usability. Steganocrypto centralize the Stepic algorithm and AES techniques to improve the transfer of text data in order to avoid forensic techniques.
+
+- Verrsion of python: python 2.7
+
+Install
+=
+
+~ pip install Steganocrypto
--- Steganocrypto.py
+++ Steganocrypto.py
@@ -1,23 +1,25 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-#Importa funcoes e algumas utilidades da biblioteca tkinter
-from Tkinter import *
+#Importa bibliotecas
 import Tkinter, Tkconstants, tkFileDialog
+import tkSimpleDialog
+import tkMessageBox
+from Tkinter import *
 from tkFileDialog import *
 from PIL import Image
 import aes #criptografia
 import stepic #esteganografia
-import tkSimpleDialog
-import tkMessageBox as box
 
 #-------------------------------------\funcoes\---------------------------------------------
+
+def but1(): print('Buttonon was pushed')
+def Save(): tkinter.filedialog.asksaveasfile()		#Define Funcao
 def Quit(): root.destroy()				#Define Funcao
+def Help(): tkinter.messagebox()
 
-def About(): tkinter.messagebox()
+#-------------------------\Criptografia\--------------------------------
 
-
-#-------------------------\funcao Criptografia\--------------------------------
 def Cripto():
 	a_texto = askopenfilename(filetypes=[("Arquivos de Texto","*.txt")])
 	f = open(a_texto)
@@ -31,26 +33,26 @@ def Cripto():
 	print crypted
 
 	return crypted
-
 #---------------------------\descriptografia\---------------------------------
 
 def Descripto():
+	
 	s = Stegana()
 	blocksize = 256
 	key = tkSimpleDialog.askstring('Criptografia AES', 'Digite a chave:')
 	txt_descripto = aes.decrypt(s  , key, blocksize )
-
-	
 	salva_text = tkFileDialog.asksaveasfile(mode='w', defaultextension=".txt")
 	salva_text.write(txt_descripto)
 
 	print 'O texto Descriptografado'
 	print txt_descripto
+
 	return Descripto
 
-#-------------------------\funções Esteganografia\----------------------------
+#-------------------------\Esteganografia\----------------------------
 
 def Stegano():
+
 	img_stegano = askopenfilename(filetypes=[("Arquivo PNG","*.png")])
 	im = Image.open(img_stegano)
 	i = Cripto()
@@ -59,7 +61,7 @@ def Stegano():
 	salva_foto = asksaveasfile(mode='w', defaultextension=".png")
 	im2.save(salva_foto)
 	
-#----------------------------\Steganalise\-----------------------------------       
+#----------------------------\ESteganalise\-----------------------------------       
 
 def Stegana():
 
@@ -77,10 +79,13 @@ def Stegana():
 root = Tk() 						#cria tabela grafica geral
 menubar = Menu(root)					#cria menu na tabela root
 root.config(menu=menubar)				# PESQUISAR
-root.title('Steganocrypto')			#configra titulo da janela
+root.title('Esteganocriptografia')			#configra titulo da janela
 
 
 filemenu = Menu(menubar)				#cria aba de menu
+filemenu2 = Menu(menubar)				#cria aba de menu
+filemenu3 = Menu(menubar)				#cria aba de menu
+filemenu4 = Menu(menubar)
 
 #Layout--------------------------------/Cabeçalho/-----------------------------------------------
 
@@ -90,8 +95,7 @@ Label (root, text="Esteganografia").grid(row=0, padx= 10, pady=10)
 
 #Layout ----------------- inferior Esteganalise------------
  
-
-#---------------\Escolher imagem p cripto\---------------------------
+#---------------\Escolher imagem para Esteganocripto\---------------------------
 
 Label (root, text='Steganocripto:').grid(row = 1, padx=10, pady=10)
 botao_cript = Button(root, text = 'Stegano', height=1, width=14)
@@ -101,7 +105,7 @@ botao_cript.configure(command = Stegano)
 #cabeçalho inferior
 Label (root, text="Esteganalise").grid(row=2, padx= 8, pady=1)
 
-#---------------\Escolha imagem p ddescripto\-----------------------------
+#---------------\Escolha imagem para descripto\-----------------------------
 
 Label (root, text='Descriptostegano:').grid(row = 3, padx=10, pady=10)
 botao_cript = Button(root, text = 'Descripto', height=1, width=14)
@@ -111,15 +115,25 @@ botao_cript.configure(command = Descripto)
 
 
 #----------------------------Configuração das opcoes d---------------------------------------#
-
-menubar.add_cascade(label='Opções', menu=filemenu)	#nomeia menu 1
-
-#------------------------------------\Atribuiçoes e configuracoes da janela\-------------------
-
-filemenu.add_command(label='Sobre', command = Quit)
+menubar.add_cascade(label='Arquivos', menu=filemenu)	#nomeia menu 1
+menubar.add_cascade(label='Opções', menu=filemenu2)	#nomeia menu 2
+menubar.add_cascade(label='Sobre', menu=filemenu3)	#nomeia menu 3
+menubar.add_cascade(label='Ajuda', menu=filemenu4)	#nomeia menu 4
 
 
-filemenu.add_command(label='Sair', command = Quit)
+#--------------------------------\Atribuiçoes e configuracoes da janela\-------------------
+filemenu.add_command(label='Abrir...', command=Cripto)
 
+filemenu.add_command(label='Salvar como...', command=Cripto)
+
+filemenu2.add_command(label='criptografia')
+
+filemenu.add_separator()
+
+filemenu2.add_command(label="esteganografia")
+
+filemenu.add_command(label='Sair', command=Quit)
+
+filemenu3.add_command(label='Sobre')
 
 root.mainloop()   #loop da janela
